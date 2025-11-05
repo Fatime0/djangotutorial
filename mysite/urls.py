6bad9ec720b0c41f0f85855570ea6ccbb6e96c19
Fatime0 <hasanzadafatima@yahoo.com>
2025-11-05 21:38:29 +0400

    .
    .

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,14 +1,27 @@
+"""
+URL configuration for mysite project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from django.views.generic import RedirectView 
 
 urlpatterns = [
-    # 💥 BU SƏTRİ ƏLAVƏ EDİN 💥
-    # Boş yolu '' (yəni /) polls tətbiqinin urls.py faylına yönəldir
-    path('', include('polls.urls')), 
+
+    path("", RedirectView.as_view(url="polls/", permanent=True)),
+    path("polls/", include("polls.urls")),
     
-    # İdarəetmə paneli üçün mövcud yol
-    path('admin/', admin.site.urls),
-    
-    # Əgər əvvəlki path('polls/', include('polls.urls')) sətri var idisə, 
-    # onu artıq silə bilərsiniz, çünki bütün / ünvanı polls-a yönəltdiniz.
+    path("admin/", admin.site.urls),
 ]
